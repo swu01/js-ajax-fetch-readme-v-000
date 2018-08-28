0f876b03765044926b8580b13a5be85823201342
Sammy Wu <swucode@gmail.com>
2018-08-28 16:30:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+
+// Token would be assigned to const token
+const token = token here
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
